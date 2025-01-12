43548c70b44474c2925ec6eb8837d75cf0bd39a5
Vivek Ajbe <vajbe.tce@gmail.com>
2025-01-12 21:30:02 +0530

    add pgx for connection

--- services/users/go.mod
+++ services/users/go.mod
@@ -2,4 +2,11 @@ module go-microservices/users
 
 go 1.22.3
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.2 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)
--- services/users/internal/handler/user_handler.go
+++ services/users/internal/handler/user_handler.go
@@ -1,13 +1,16 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"go-microservices/users/internal/middleware/response"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v5"
 )
 
 type UserHandler struct{}
@@ -28,6 +31,26 @@ func init() {
 }
 
 func (h *UserHandler) GetUsers(w http.ResponseWriter, r *http.Request) {
+
+	urlExample := "postgres://admin:admin@localhost:5432/admin"
+	conn, err := pgx.Connect(context.Background(), urlExample)
+	//  conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var name string
+	var weight int64
+	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		/* os.Exit(1) */
+	}
+
+	fmt.Println(name, weight)
+
 	response.Success(w, "Users have been retrieved successfully.", users)
 }
 
