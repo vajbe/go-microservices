cf948036466793f233bb8615445fbad120d5229d
Vivek Ajbe <vajbe.tce@gmail.com>
2025-01-15 10:18:22 +0530

    udpate types

--- services/users/internal/types/users.go
+++ services/users/internal/types/users.go
@@ -3,7 +3,8 @@ package types
 import "time"
 
 type User struct {
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	CreatedAt time.Time
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	Id        string    `json:"id"`
 }
