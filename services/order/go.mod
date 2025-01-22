580128a4dff8629dcfcad4fbfc87b2be873a3671
Vivek Ajbe <vajbe.tce@gmail.com>
2025-01-22 21:14:50 +0530

    add kafka dependency

--- services/order/go.mod
+++ services/order/go.mod
@@ -15,8 +15,11 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.2 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/redis/go-redis/v9 v9.7.0 // indirect
+	github.com/segmentio/kafka-go v0.4.47 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
--- services/order/main.go
+++ services/order/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"go-microservices/order/config"
 	"go-microservices/order/db"
+	"go-microservices/order/kafka"
 	"go-microservices/order/middleware"
 	"go-microservices/order/routes"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/mux"
 	"github.com/redis/go-redis/v9"
@@ -21,11 +26,16 @@ func main() {
 	// Load configuration
 	SERVICE_CONFIG = config.Load()
 
-	// Initialize Table and dbs
+	// Initialize Table and DBs
 	db.InitializeDb(SERVICE_CONFIG)
 
-	//	Initialize Redis
+	// Initialize Redis
 	db.InitRedis(SERVICE_CONFIG)
+
+	// Initialize Kafka (producer and consumer)
+	kafkaCtx, kafkaCancel := context.WithCancel(context.Background())
+	defer kafkaCancel()
+	producer, consumer := InitKafka(kafkaCtx)
 
 	// Initialize router
 	router := mux.NewRouter()
@@ -37,6 +47,46 @@ func main() {
 	routes.RegisterOrderRoutes(router)
 
 	// Start server
+	server := &http.Server{
+		Addr:    ":" + SERVICE_CONFIG.Port,
+		Handler: router,
+	}
+
+	// Handle termination signal
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-stopChan
+		log.Println("Shutting down gracefully...")
+		kafkaCancel() // Stop Kafka consumer
+		consumer.Close()
+		producer.Close()
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatalf("HTTP server Shutdown: %v", err)
+		}
+	}()
+
 	log.Printf("Order Service running on port %s", SERVICE_CONFIG.Port)
-	log.Fatal(http.ListenAndServe(":"+SERVICE_CONFIG.Port, router))
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+}
+
+func InitKafka(ctx context.Context) (*kafka.KafkaProducer, *kafka.KafkaConsumer) {
+	// Kafka broker and topic configuration
+	brokers := []string{"localhost:9092"}
+	topic := "orders"
+
+	// Initialize producer
+	producer := kafka.NewKafkaProducer(brokers, topic)
+
+	// Initialize consumer
+	consumer := kafka.NewKafkaConsumer(brokers, topic, "order-group")
+
+	// Start consuming in the background
+	go consumer.Consume(ctx)
+
+	return producer, consumer
 }
